ba3192f988d107821bb9ca2b839eaf3539e428cc
Dima <dmitry.shakhtarin@fluence.ai>
2023-12-25 19:09:53 +0300

    delete legacy import, pass new improts to compiler (#74)

--- server/src/validation.ts
+++ server/src/validation.ts
@@ -2,8 +2,7 @@ import * as fs from 'fs';
 import * as Path from 'path';
 
 import type { TextDocument } from 'vscode-languageserver-textdocument';
-import { Diagnostic, DiagnosticSeverity, RemoteConsole } from 'vscode-languageserver/node';
-import type { WorkspaceFolder } from 'vscode-languageserver-protocol';
+import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver/node';
 
 import {
     AquaLSP,
@@ -14,104 +13,6 @@ import {
 } from '@fluencelabs/aqua-language-server-api/aqua-lsp-api';
 
 import type { Settings } from './settings';
-
-function findNearestNodeModules(fileLocation: string, projectLocation: string): string | undefined {
-    const relative = Path.relative(projectLocation, fileLocation);
-
-    const projectPath = Path.resolve(projectLocation.replace('file://', ''));
-
-    // project location is a part of file location
-    if (relative && !relative.startsWith('..') && !Path.isAbsolute(relative)) {
-        let currentPath = Path.join(fileLocation.replace('file:/', ''), '..');
-
-        let result: string | undefined = undefined;
-
-        while (true) {
-            const possibleNodeModulesPath = Path.join(currentPath, '/node_modules');
-            if (fs.existsSync(possibleNodeModulesPath)) {
-                result = Path.resolve(possibleNodeModulesPath);
-                break;
-            }
-            if (Path.resolve(currentPath) === projectPath) {
-                break;
-            } else {
-                currentPath = Path.join(currentPath, '..');
-            }
-        }
-
-        return result;
-    } else {
-        return undefined;
-    }
-}
-
-function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-    return value !== null && value !== undefined;
-}
-
-function getImports(
-    settings: Settings,
-    textDocument: TextDocument,
-    folders: WorkspaceFolder[],
-    console: RemoteConsole,
-) {
-    let imports: string[] = [];
-
-    const assumeImports = settings.enableLegacyAutoImportSearch;
-
-    const openFolders = folders.map((f) => f.uri.replace('file://', ''));
-
-    // add all workspace folders to imports
-    // 1. open folders
-    if (assumeImports) imports = imports.concat(openFolders);
-
-    // 2. imports from settings
-    if (settings.imports && Array.isArray(settings.imports)) {
-        const validatedImports: string[] = settings.imports.filter((s) => {
-            const isString = typeof s == 'string';
-            if (!isString) {
-                console.warn(
-                    `Field 'import' in extension settings must have only array of strings. 
-                    Cannot add import ${JSON.stringify(s)}`,
-                );
-            }
-
-            return isString;
-        });
-
-        const absoluteImports = validatedImports.filter((s) => Path.isAbsolute(s));
-
-        // relative imports must be started from open folders
-        const relativeImports = validatedImports
-            .filter((s) => !absoluteImports.includes(s))
-            .map((s) => openFolders.map((f) => Path.join(f, s)))
-            .flat();
-
-        imports = imports.concat(relativeImports);
-        imports = imports.concat(absoluteImports);
-
-        if (assumeImports) {
-            imports = imports.concat(relativeImports.map((s) => Path.join(s, '/node_modules')));
-            imports = imports.concat(absoluteImports.map((s) => Path.join(s, '/node_modules')));
-        }
-    }
-
-    if (assumeImports) {
-        // 3. node_modules in open folders
-        imports = imports.concat(openFolders.map((f) => Path.join(f, '/node_modules')));
-
-        // 4. path to aqua library
-        if (require.main) {
-            imports = imports.concat(require.main.paths);
-        }
-
-        // 5. node_modules from root to open folders
-        const nodeModulesPaths = folders.map((f) => findNearestNodeModules(textDocument.uri, f.uri)).filter(notEmpty);
-        imports = imports.concat(nodeModulesPaths);
-    }
-
-    return imports;
-}
 
 function infoToDiagnostic(textDocument: TextDocument, info: ErrorInfo | WarningInfo): Diagnostic {
     const severity = info.infoType === 'error' ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning;
@@ -145,22 +46,20 @@ function infoToDiagnostic(textDocument: TextDocument, info: ErrorInfo | WarningI
 export async function compileAqua(
     settings: Settings,
     textDocument: TextDocument,
-    folders: WorkspaceFolder[],
-    console: RemoteConsole,
 ): Promise<[Diagnostic[], TokenLink[], TokenInfo[]]> {
     const uri = textDocument.uri.replace('file://', '');
 
-    const imports = getImports(settings, textDocument, folders, console);
-
     // compile aqua and get result
-    const result = await AquaLSP.compile(uri, imports);
+    const result = await AquaLSP.compile(uri, settings.imports);
 
     const diagnostics: Diagnostic[] = [];
     const links: TokenLink[] = [];
 
     const docPath = Path.parse(textDocument.uri);
-    const linksSearch = [docPath.dir.replace('file://', '')].concat(imports);
 
+    let linksSearch = [docPath.dir.replace('file://', '')];
+
+    // TODO: fix import locations search
     result.importLocations.map(function (ti) {
         const path = linksSearch.map((i) => Path.join(i, ti.path)).find(fs.existsSync);
 
