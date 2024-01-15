0370133c5430f773abc463d9398aae37ab263ca3
InversionSpaces <inversionspaces@vivaldi.net>
2024-01-15 11:18:04 +0000

    Add npm test

--- test/src/extension.test.ts
+++ test/src/extension.test.ts
@@ -11,11 +11,11 @@ function delay(ms: number) {
 }
 
 suite('Extension Test Suite', () => {
-    test('Should provide diagnostics for syntax errors', async () => {
+    async function openDocument(relPath: string): Promise<vscode.TextDocument> {
         assert.ok(vscode.workspace.workspaceFolders?.length === 1, 'Test workspace should contain one folder');
 
         const rootPath = vscode.workspace.workspaceFolders[0]!.uri.fsPath;
-        const filePath = path.join(rootPath, 'oneFile/file.aqua');
+        const filePath = path.join(rootPath, relPath);
         const fileUri = vscode.Uri.file(filePath);
 
         const document = await vscode.workspace.openTextDocument(fileUri);
@@ -30,6 +30,12 @@ suite('Extension Test Suite', () => {
             'Aqua extension is not active',
         );
 
+        return document;
+    }
+
+    test('Semantic errors in single file', async () => {
+        const document = await openDocument('singleFile/file.aqua');
+
         // Wait for the extension to provide diagnostics
         await delay(2000);
 
@@ -39,7 +45,31 @@ suite('Extension Test Suite', () => {
         assert.ok(diagnostics.length > 0, 'No diagnostics provided');
         assert.ok(
             diagnostics.find((diagnostic) => {
-                return diagnostic.severity === vscode.DiagnosticSeverity.Error;
+                return (
+                    diagnostic.severity === vscode.DiagnosticSeverity.Error &&
+                    diagnostic.message.includes('Wrong value type')
+                );
+            }),
+            'No error diagnostics provided',
+        );
+    }).timeout(10000);
+
+    test('Semantic errors in npm package', async () => {
+        const document = await openDocument('npmPackage/main.aqua');
+
+        // Wait for the extension to provide diagnostics
+        await delay(2000);
+
+        // Retrieve diagnostics
+        const diagnostics = vscode.languages.getDiagnostics(document.uri);
+
+        assert.ok(diagnostics.length > 0, 'No diagnostics provided');
+        assert.ok(
+            diagnostics.find((diagnostic) => {
+                return (
+                    diagnostic.severity === vscode.DiagnosticSeverity.Error &&
+                    diagnostic.message.includes('Wrong value type')
+                );
             }),
             'No error diagnostics provided',
         );
